00c6e6bf9c1561e40598f29b22c8d2f75259756a
noname <you@example.com>
2021-07-11 17:24:15 +0800

    authed_only

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show', 'index']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
